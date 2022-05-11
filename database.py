9212f60cc938dc5d0cab00367e8813ff9b858e27
SaiKrishnanRU <104107254+SaiKrishnanRU@users.noreply.github.com>
2022-05-12 00:41:30 +0530

    CU-21ff7rt
    #CU-21ff7rt

---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def userDb():
+    conn = sqlite3.connect('dashboard.sqlite')
+    return conn
+
+
+def sourceDb():
+    conn = sqlite3.connect('/home/vadata/marketfeedsv2/active_counts.sqlite')
+    return conn
